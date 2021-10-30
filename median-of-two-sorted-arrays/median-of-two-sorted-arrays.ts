e94dea2ea5a15f05dd6885b90be59c554b828f18
Yok <49203200+Yokk1e@users.noreply.github.com>
2021-10-31 01:19:56 +0700

    Update median-of-two-sorted-arrays.ts

--- median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
+++ median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
@@ -1,12 +1,8 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
     const arrGroup = [...nums1, ...nums2].sort((a, b) => a - b);
       if (arrGroup.length % 2 === 0) {
-        if (arrGroup.length === 2) {
-          return (arrGroup[0] + arrGroup[1]) / 2;
-        } else {
           const middleIndex = (arrGroup.length / 2) - 1;
           return (arrGroup[middleIndex] + arrGroup[middleIndex + 1]) / 2;
-        }
       } else {
         const middleIndex = Math.floor(arrGroup.length / 2);
         return arrGroup[middleIndex];
